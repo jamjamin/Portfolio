4ff80b50bb9a315aba2252c7a4f85918321702fa
jamjamin <benjamjamin02@gmail.com>
2024-08-07 17:06:38 +1200

    Carousel Prototype

--- portfront/src/components/projects/ImgDemo.tsx
+++ portfront/src/components/projects/ImgDemo.tsx
@@ -1,25 +1,57 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
+
+import "swiper/css"
+import "swiper/css/effect-coverflow"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+
+import "@/styles/ProjectDetails.css"
 
 export const ImgDemo = () => {
+    const width = 480;
+    const height = 270;
+
     return (
-        <div className='img-demo'>
-            <Image 
-                width={480}
-                height={270}
+        <Swiper
+            navigation={true}
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={3}
+            onSlideChange={() => console.log('Slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            pagination={{ type: 'bullets', clickable: true}}
+        >
+            <SwiperSlide><Image 
+                width={width}
+                height={height}
                 src="/test.png" alt={''}                
-            />
-            <Image 
-                width={480}
-                height={270}
+            /></SwiperSlide>
+            <SwiperSlide><Image 
+                width={width}
+                height={height}
                 src="/test.png" alt={''}                
-            />
-            <Image 
-                width={480}
-                height={270}
+            /></SwiperSlide>
+            <SwiperSlide><Image 
+                width={width}
+                height={height}
                 src="/test.png" alt={''}                
-            />
-        </div>
+            /></SwiperSlide>
+            <SwiperSlide><Image 
+                width={width}
+                height={height}
+                src="/test.png" alt={''}                
+            /></SwiperSlide>
+            <SwiperSlide><Image 
+                width={width}
+                height={height}
+                src="/test.png" alt={''}                
+            /></SwiperSlide>
+        </Swiper>
     )
 }
 
