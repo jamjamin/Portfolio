a0be1de8e6e91922d086e83859a612973ae04870
jamjamin <benjamjamin02@gmail.com>
2024-08-08 14:32:58 +1200

    Filter Start

--- portfront/src/app/projects/page.tsx
+++ portfront/src/app/projects/page.tsx
@@ -9,8 +9,32 @@ export default function Projects() {
     return (
         <div>
             <h1> Projects </h1>
-            <div> 
-                <input type="text"/>
+            <div className='project-filters'> 
+                <input type="text" placeholder='Search Project' className='search-project'/>
+                <div className='filter-selection'>
+                    Filters 
+                    <select name="type">
+                        <option> Website </option>
+                        <option> Desktop </option>
+                        <option> Mobile </option>
+                    </select>
+                    <select name="category">
+                        <option> Collaborative </option>
+                        <option> Experimental </option>
+                        <option> University </option>
+                    </select>
+                    <select name="tech">
+                        <option> NextJS </option>
+                        <option> ReactJS </option>
+                        <option> NodeJS </option>
+                    </select>
+                    <select name="language">
+                        <option> Java </option>
+                        <option> Python </option>
+                        <option> JavaScript </option>
+                        <option> TypeScript </option>
+                    </select>
+                </div>
             </div>
             <div className='project-list'>
                 <ProjectItem projectData={projectData[0]}/>
